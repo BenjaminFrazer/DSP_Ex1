55aa81a07e05a83e6408da3af6f929b7aa490358
peter7417 <partheepan_s@live.com>
2021-10-10 18:14:18 +0100

    Cleaned up voice_enhancer.py code Changed voweldetector.py to compare regular fft than log(fft)

--- voweldetector.py
+++ voweldetector.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
 
-def identifier(list, vowel, dB):
+def identifier(list, vowel, data):
     j = 0
     indexList = []
     while j <= len(list)-1:
@@ -13,9 +13,11 @@ def identifier(list, vowel, dB):
         j += 1
 
     for k in indexList:
-        if dB[k] > dB[k-1] and dB[k] > dB[k+1] and dB[k] > dB[k-2] and dB[k] > dB[k+2] and dB[k] > dB[k-3] \
-                and dB[k] > dB[k+3] and dB[k] > dB[k-4] and dB[k] > dB[k+4]:
+        if data[k] > data[k-1] and data[k] > data[k+1] and data[k] > data[k-2] and data[k] > data[k+2] \
+                and data[k] > data[k-3] and data[k] > data[k+3] and data[k] > data[k-4] and data[k] > data[k+4]:
             return 1
+        else:
+            return 0
 
 
 def voweldetector(name):
@@ -42,7 +44,6 @@ def voweldetector(name):
     """Import an audio file in .wav format at 48KHz"""
     path = os.getcwd()
     file_name = name
-    # file_name = 'FFT_2.wav'
 
     location = os.path.join(path, file_name)
     samplerate, data = wavfile.read(location)
@@ -50,16 +51,11 @@ def voweldetector(name):
     """Create the FFT of the data(np array of numbers) obtained from the audio file"""
     data_fft = np.fft.fft(data)
 
-    """Create an array of half range of values from the fourier transform so we can plot up to the nyquist frequency"""
-    half_rangeValues = data_fft[0:int(len(data_fft) / 2 - 1)]
-
     """Define the frequency and time domains"""
-    f = np.linspace(0, samplerate / 2, len(half_rangeValues))
+    f = np.linspace(0, samplerate/2, len(data_fft))
 
     """Plot the Frequency domain spectrum"""
-    dB = 20 * np.log10(half_rangeValues / max(half_rangeValues))
-    plt.plot(f, dB)
-    plt.xscale('log')
+    plt.plot(f, np.abs(data_fft))
     plt.title('Frequency Domain')
     plt.xlabel('Frequency(rad/s)')
     plt.ylabel('Amplitude(dB)')
@@ -68,7 +64,7 @@ def voweldetector(name):
     frequencyList = np.round(f, 1)
 
     while v <= len(vowels)-1:
-        p = (identifier(frequencyList, vowels[v], dB))
+        p = (identifier(frequencyList, vowels[v], np.abs(data_fft)))
         if p == 1:
             foundVowels.append(vowels_name[v])
         v += 1
@@ -76,9 +72,11 @@ def voweldetector(name):
     return foundVowels
 
 
-
 vowel_1 = voweldetector('Test_recording.wav')
-vowel_2 = voweldetector('FFt Test.wav')
+vowel_2 = voweldetector('Fox.wav')
 
-print(vowel_1)
-print(vowel_2)
+for vowel in vowel_1:
+    print('Wave file 1: ',vowel)
+
+for vowel in vowel_2:
+    print('Wave file 2: ',vowel)
